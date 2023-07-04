3d2d106bb7eaba906ae23dfbd085b5ce2274a879
susan040 <136952451+susan040@users.noreply.github.com>
2023-07-04 13:15:16 +0545

    first commit

---
+++ todo/lib/data/data.dart
@@ -0,0 +1,5 @@
+import 'package:todo/model/model.dart';
+
+final List<ToDo> toDoData =[
+  ToDo(name: "Susan", status: "Living my life in my style.")
+];
---
+++ todo/lib/model/model.dart
@@ -0,0 +1,8 @@
+class ToDo{
+  String name;
+  String status;
+  ToDo({
+    required this.name,
+    required this.status
+  });
+}
